cb5098c98ccef6f8e31893c2f2eca28e54488574
ikedayuji <ikedayuji.2002@gmail.com>
2024-08-15 23:46:26 -0300

    Projeto com comentário

--- Chat TCP/client.py
+++ Chat TCP/client.py
@@ -1,40 +1,39 @@
 import socket
 import threading
 
-# Choosing Nickname
-nickname = input("Choose your nickname: ")
+# Escolhendo um Apelido
+nickname = input("Choose your nickname: ")  # Solicita ao usuário que escolha um apelido
 
-# Connecting To Server
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55555))
+# Conectando ao Servidor
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Cria um socket de cliente usando IPv4 e TCP
+client.connect(('127.0.0.1', 55555))  # Conecta o cliente ao servidor usando o endereço IP e a porta especificados
 
-# Listening to Server and Sending Nickname
+# Função para Ouvir o Servidor e Enviar o Apelido
 def receive():
     while True:
         try:
-            # Receive Message From Server
-            # If 'NICK' Send Nickname
-            message = client.recv(1024).decode('ascii')
-            if message == 'NICK':
-                client.send(nickname.encode('ascii'))
+            # Recebe a Mensagem do Servidor
+            message = client.recv(1024).decode('ascii')  # Recebe uma mensagem do servidor (até 1024 bytes) e decodifica para ASCII
+            # Se a Mensagem For 'NICK', Envia o Apelido
+            if message == 'NICK':  # Verifica se a mensagem recebida é uma solicitação de apelido
+                client.send(nickname.encode('ascii'))  # Envia o apelido do cliente para o servidor
             else:
-                print(message)
+                print(message)  # Caso contrário, exibe a mensagem recebida no console
         except:
-            # Close Connection When Error
-            print("An error occured!")
-            client.close()
-            break
-        
-# Sending Messages To Server
+            # Fecha a Conexão em Caso de Erro
+            print("An error occured!")  # Exibe uma mensagem de erro
+            client.close()  # Fecha a conexão com o servidor
+            break  # Encerra o loop e a função
+
+# Função para Enviar Mensagens ao Servidor
 def write():
     while True:
-        message = '{}: {}'.format(nickname, input(''))
-        client.send(message.encode('ascii'))
+        message = '{}: {}'.format(nickname, input(''))  # Formata a mensagem incluindo o apelido do usuário
+        client.send(message.encode('ascii'))  # Envia a mensagem para o servidor, codificada em ASCII
         
-# Starting Threads For Listening And Writing
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
+# Iniciando Threads para Ouvir e Escrever
+receive_thread = threading.Thread(target=receive)  # Cria uma nova thread para a função 'receive' (ouvir as mensagens do servidor)
+receive_thread.start()  # Inicia a thread de recepção
 
-write_thread = threading.Thread(target=write)
-write_thread.start()
-     
+write_thread = threading.Thread(target=write)  # Cria uma nova thread para a função 'write' (enviar as mensagens ao servidor)
+write_thread.start()  # Inicia a thread de escrita
--- Chat TCP/server.py
+++ Chat TCP/server.py
@@ -1,62 +1,61 @@
 import socket
 import threading
 
-# Connection Data
-host = '127.0.0.1'
-port = 55555
+# Dados de Conexão
+host = '127.0.0.1'  # Endereço IP do servidor (localhost neste caso)
+port = 55555        # Porta em que o servidor irá escutar as conexões
 
-# Starting Server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
+# Iniciando o Servidor
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Cria um socket de servidor usando IPv4 e TCP
+server.bind((host, port))  # Vincula o servidor ao endereço IP e à porta especificados
+server.listen()  # Coloca o servidor em modo de escuta para aceitar conexões
 
-# Lists For Clients and Their Nicknames
-clients = []
-nicknames = []
+# Listas Para Armazenar Clientes e Seus Apelidos
+clients = []  # Lista para armazenar os sockets dos clientes conectados
+nicknames = []  # Lista para armazenar os apelidos dos clientes conectados
 
-# Sending Messages To All Connected Clients
+# Enviando Mensagens Para Todos os Clientes Conectados
 def broadcast(message):
-    for client in clients:
-        client.send(message)
-        
-# Handling Messages From Clients
+    for client in clients:  # Itera sobre a lista de clientes conectados
+        client.send(message)  # Envia a mensagem para cada cliente
+
+# Tratando Mensagens Recebidas dos Clientes
 def handle(client):
     while True:
         try:
-            # Broadcasting Messages
-            message = client.recv(1024)
-            broadcast(message)
+            # Recebendo e Broadcast de Mensagens
+            message = client.recv(1024)  # Recebe a mensagem do cliente (até 1024 bytes)
+            broadcast(message)  # Envia a mensagem para todos os clientes conectados
         except:
-            # Removing And Closing Clients
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            broadcast('{} left!'.format(nickname).encode('ascii'))
-            nicknames.remove(nickname)
-            break
-        
-# Receiving / Listening Function
+            # Removendo e Fechando Conexões de Clientes
+            index = clients.index(client)  # Obtém o índice do cliente na lista
+            clients.remove(client)  # Remove o cliente da lista
+            client.close()  # Fecha a conexão do cliente
+            nickname = nicknames[index]  # Obtém o apelido do cliente usando o índice
+            broadcast('{} left!'.format(nickname).encode('ascii'))  # Notifica a saída do cliente para os outros
+            nicknames.remove(nickname)  # Remove o apelido da lista
+            break  # Sai do loop e encerra a função
+
+# Função Para Receber e Escutar Conexões
 def receive():
     while True:
-        # Accept Connection
-        client, address = server.accept()
-        print("Connected with {}".format(str(address)))
+        # Aceitar a Conexão
+        client, address = server.accept()  # Aceita uma nova conexão e obtém o socket e o endereço do cliente
+        print("Connected with {}".format(str(address)))  # Exibe o endereço do cliente conectado
 
-        # Request And Store Nickname
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
-        nicknames.append(nickname)
-        clients.append(client)
+        # Solicitar e Armazenar o Apelido
+        client.send('NICK'.encode('ascii'))  # Envia uma mensagem pedindo o apelido do cliente
+        nickname = client.recv(1024).decode('ascii')  # Recebe o apelido do cliente
+        nicknames.append(nickname)  # Adiciona o apelido à lista de apelidos
+        clients.append(client)  # Adiciona o cliente à lista de clientes conectados
 
-        # Print And Broadcast Nickname
-        print("Nickname is {}".format(nickname))
-        broadcast("{} joined!".format(nickname).encode('ascii'))
-        client.send('Connected to server!'.encode('ascii'))
+        # Exibir e Enviar o Apelido para Todos
+        print("Nickname is {}".format(nickname))  # Exibe o apelido no servidor
+        broadcast("{} joined!".format(nickname).encode('ascii'))  # Informa a todos que um novo cliente se conectou
+        client.send('Connected to server!'.encode('ascii'))  # Confirma a conexão para o cliente
 
-        # Start Handling Thread For Client
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
+        # Iniciar uma Thread Para Tratar o Cliente
+        thread = threading.Thread(target=handle, args=(client,))  # Cria uma nova thread para lidar com o cliente
+        thread.start()  # Inicia a thread
         
-receive()
-
+receive()  # Chama a função para começar a receber conexões
