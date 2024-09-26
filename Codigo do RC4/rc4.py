8d3ddbe1ade84549fb93bfed41e09741207f724a
ikedayuji <ikedayuji.2002@gmail.com>
2024-09-26 18:05:04 -0300

    Algumas Correções do RC4

--- Codigo do RC4/rc4.py
+++ Codigo do RC4/rc4.py
@@ -28,29 +28,19 @@ def rc4(key, text):
     key_ascii = [ord(c) for c in key]
 
     # Impressão dos resultados
-    print("Entrada:")
     print(f"\tTexto Plano = \"{text}\";")
     print(f"\n\tTexto Plano ASCII = {text_ascii};")
     print(f"\n\tChave= {key}")
     print(f"\n\tChave ASCII = {key_ascii};")
     print(f"\n\tFluxo de chave gerado = {key_stream};")
     print(f"\n\tTexto Cript. = {encrypted_text};")
+    print("\n" + "-" * 40 + "\n")
     
     return ''.join(result)
 
-# Teste da função
-rc4_key_1 = "D&Ot)[YW"
-plaintext_1 = "Cybersecurity melhor disciplina do curso."
-rc4(rc4_key_1, plaintext_1)
+# Solicitar entrada do usuário
+plaintext = input("Digite o Texto Plano: ")
+key = input("Digite a Chave: ")
 
-print("\n" + "-" * 40 + "\n")
-
-rc4_key_2 = "$@C*9)6C{4^dXNw>H#W,be/'L2pM8r;JY?x}B]@A`T!q?iO`=n.Lgm(3z8@S[u]dY1k|%RI!MP-(FtZl&^3:jnK<TG6[5Jw}"
-plaintext_2 = "Cybersecurity melhor disciplina do curso."
-rc4(rc4_key_2, plaintext_2)
-
-print("\n" + "-" * 40 + "\n")
-
-rc4_key_3 = "!M|7s]u^{DFj^?8+fL:0Z!*%1P_3B}9m~V0@H^Qf7y&Z4Wb>kS^T<d.$.pL@R|g)x)-6(E&h%T-}(W%z{U9mZz8~m8BfP!c&@k7I\\5I~T_vD!4A>|oO[}3*T|$?e~0]V5&y@r1X2k+@T]j?|2|Q%}R,D)Up\\8gM;W}|7eNFk^t.h/j;6#y-!t5)\\^LJ[7S<4A,f$Ks1|&sX!w*G(Z@i>jE>6~]oA5]k'.:o=7n9h)$J_!aB{N-Jb1M}NzD\\*h"
-plaintext_3 = "Cybersecurity melhor disciplina do curso."
-rc4(rc4_key_3, plaintext_3)
+# Executar a função com a entrada do usuário
+rc4(key, plaintext)
